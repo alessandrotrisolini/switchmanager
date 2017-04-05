7f3a2d75fb9b9c0775fa6f5c8503464a85e8cdf7
Alessandro Trisolini <ale.triso@gmail.com>
2017-04-05 11:17:09 +0200

    Reorder of cli

--- managerd/cli/cli.go
+++ managerd/cli/cli.go
@@ -41,78 +41,100 @@ func newLine(c *color.Color, r *bufio.Reader) []string {
 	return args
 }
 
+func run(args []string) bool {
+	if len(args) == 3 &&
+		args[1] == "-address" &&
+		cmn.CheckIPAndPort(args[2]) {
+		if ms.IsAgentRegistred(args[2]) {
+			a := createAgentd(args[2])
+			a.InstantiateProcessPOST()
+		} else {
+			log.Error("Agent @", args[2], "in not registred")
+		}
+		return true
+	}
+	return false
+}
+
+func kill(args []string) bool {
+	var pid int
+	if len(args) == 5 &&
+		args[1] == "-address" &&
+		cmn.CheckIPAndPort(args[2]) &&
+		args[3] == "-pid" &&
+		cmn.CheckPID(args[4], &pid) {
+		if ms.IsAgentRegistred(args[2]) {
+			a := createAgentd(args[2])
+			a.KillProcessPOST(pid)
+		} else {
+			log.Error("Agent @", args[2], "in not registred")
+		}
+		return true
+	}
+	return false
+}
+
+func dump(args []string) bool {
+	if len(args) == 3 &&
+		args[1] == "-address" &&
+		cmn.CheckIPAndPort(args[2]) {
+		if ms.IsAgentRegistred(args[2]) {
+			a := createAgentd(args[2])
+			a.DumpProcessesGET()
+		} else {
+			log.Error("Agent @", args[2], "in not registred")
+		}
+		return true
+	}
+	return false
+}
+
+func list() {
+	agents, err := ms.RegistredAgents()
+	if err != nil {
+		log.Error(err)
+	} else {
+		if len(agents) == 0 {
+			log.Info("No agents have been registred")
+		} else {
+			fmt.Println(strings.Repeat("-", 48))
+			fmt.Println("|               REGISTRED AGENTS               |")
+			for k, v := range agents {
+				fmt.Println(strings.Repeat("-", 48))
+				fmt.Println("| IP ADDRESS:", k, strings.Repeat(" ", 48-(13+len(k)+4)), "|")
+				fmt.Println("| PORT      :", v.AgentPort,
+					strings.Repeat(" ", 48-(13+len(v.AgentPort)+4)), "|")
+				fmt.Println("| OvS       :", v.OpenvSwitch,
+					strings.Repeat(" ", 48-(13+len(v.OpenvSwitch)+4)), "|")
+				fmt.Println("| INTERFACES:", v.Interfaces[0],
+					strings.Repeat(" ", 48-(13+len(v.Interfaces[0])+4)), "|")
+				for _, ifc := range v.Interfaces[1:] {
+					fmt.Println("|", strings.Repeat(" ", 11), ifc,
+						strings.Repeat(" ", 48-(13+len(ifc)+4)), "|")
+				}
+			}
+			fmt.Println(strings.Repeat("-", 48))
+		}
+	}
+}
+
 // Parse and execute commands fed to managercli (when running)
 func doCmd(args []string) {
 	switch args[0] {
 	case "run":
-		if len(args) == 3 &&
-			args[1] == "-address" &&
-			cmn.CheckIPAndPort(args[2]) {
-			if ms.IsAgentRegistred(args[2]) {
-				a := createAgentd(args[2])
-				a.InstantiateProcessPOST()
-			} else {
-				log.Error("Agent @", args[2], "in not registred")
-			}
-		} else {
+		if !run(args) {
 			log.Error("Syntax: run -address <ip:port>")
 		}
 	case "kill":
-		var pid int
-		if len(args) == 5 &&
-			args[1] == "-address" &&
-			cmn.CheckIPAndPort(args[2]) &&
-			args[3] == "-pid" &&
-			cmn.CheckPID(args[4], &pid) {
-			if ms.IsAgentRegistred(args[2]) {
-				a := createAgentd(args[2])
-				a.KillProcessPOST(pid)
-			} else {
-				log.Error("Agent @", args[2], "in not registred")
-			}
-		} else {
+		if !kill(args) {
 			log.Error("Syntax: kill -address <ip:port> -pid <PID>")
 		}
 	case "dump":
-		if len(args) == 3 &&
-			args[1] == "-address" &&
-			cmn.CheckIPAndPort(args[2]) {
-			if ms.IsAgentRegistred(args[2]) {
-				a := createAgentd(args[2])
-				a.DumpProcessesGET()
-			} else {
-				log.Error("Agent @", args[2], "in not registred")
-			}
-		} else {
+		if !dump(args) {
 			log.Error("Syntax: dump -address <ip:port>")
 		}
 	case "list":
-		agents, err := ms.RegistredAgents()
-		if err != nil {
-			log.Error(err)
-		} else {
-			if len(agents) == 0 {
-				log.Info("No agents have been registred")
-			} else {
-				fmt.Println(strings.Repeat("-", 48))
-				fmt.Println("|               REGISTRED AGENTS               |")
-				for k, v := range agents {
-					fmt.Println(strings.Repeat("-", 48))
-					fmt.Println("| IP ADDRESS:", k, strings.Repeat(" ", 48-(13+len(k)+4)), "|")
-					fmt.Println("| PORT      :", v.AgentPort,
-						strings.Repeat(" ", 48-(13+len(v.AgentPort)+4)), "|")
-					fmt.Println("| OvS       :", v.OpenvSwitch,
-						strings.Repeat(" ", 48-(13+len(v.OpenvSwitch)+4)), "|")
-					fmt.Println("| INTERFACES:", v.Interfaces[0],
-						strings.Repeat(" ", 48-(13+len(v.Interfaces[0])+4)), "|")
-					for _, ifc := range v.Interfaces[1:] {
-						fmt.Println("|", strings.Repeat(" ", 11), ifc,
-							strings.Repeat(" ", 48-(13+len(ifc)+4)), "|")
-					}
-				}
-				fmt.Println(strings.Repeat("-", 48))
-			}
-		}
+		list()
 	case "":
 	default:
 		log.Error("Invalid command")
