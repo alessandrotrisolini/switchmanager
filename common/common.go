c77d85a3f51e494fe6e221936d97a5beeba6acc2
Alessandro Trisolini <ale.triso@gmail.com>
2017-04-27 16:50:06 +0200

    Improved TLS configuration

--- common/common.go
+++ common/common.go
@@ -101,6 +101,38 @@ func TrimSuffix(s, suffix string) string {
 	return s
 }
 
+func commonTLSConfig(caCertPath string) (*tls.Config, error) {
+
+	ca, err := ioutil.ReadFile(caCertPath)
+	if err != nil {
+		return nil, err
+	}
+
+	caCertPool := x509.NewCertPool()
+	if !caCertPool.AppendCertsFromPEM(ca) {
+		return nil, errors.New("Can not load CA certificate")
+	}
+
+	tlsConfig := &tls.Config{
+		MinVersion:             tls.VersionTLS12,
+		SessionTicketsDisabled: true,
+		RootCAs:                caCertPool,
+	}
+
+	/*
+		tlsConfig.CipherSuites = []uint16{tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256}
+	*/
+
+	return tlsConfig, nil
+}
+
 //SetupTLSClient initializes a TLS client
 func SetupTLSClient(client *http.Client, certPath string, keyPath string, caCertPath string) error {
 	cert, err := tls.LoadX509KeyPair(certPath, keyPath)
@@ -108,18 +140,12 @@ func SetupTLSClient(client *http.Client, certPath string, keyPath string, caCert
 		return err
 	}
 
-	ca, err := ioutil.ReadFile(caCertPath)
+	tlsConfig, err := commonTLSConfig(caCertPath)
 	if err != nil {
 		return err
 	}
 
-	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(ca)
-
-	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{cert},
-		RootCAs:      caCertPool,
-	}
+	tlsConfig.Certificates = []tls.Certificate{cert}
 	tlsConfig.BuildNameToCertificate()
 
 	transport := &http.Transport{TLSClientConfig: tlsConfig}
@@ -140,11 +166,13 @@ func SetupTLSServer(server *http.Server, caCertPath string) error {
 		return errors.New("Can not load CA certificate")
 	}
 
-	tlsConfig := &tls.Config{
-		RootCAs:    caCertPool,
-		ClientCAs:  caCertPool,
-		ClientAuth: tls.RequireAndVerifyClientCert,
+	tlsConfig, err := commonTLSConfig(caCertPath)
+	if err != nil {
+		return err
 	}
+
+	tlsConfig.ClientCAs = tlsConfig.RootCAs
+	tlsConfig.ClientAuth = tls.RequireAndVerifyClientCert
 
 	tlsConfig.BuildNameToCertificate()
 
